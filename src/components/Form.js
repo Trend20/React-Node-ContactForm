cd2d4c55774b5bea0ecc92df59715eb24a22971f
Trend20 <enockomondi305@gmail.com>
2020-06-02 07:46:15 -0400

    input classnames

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,15 +8,24 @@ class Form extends Component {
                 <form id="contact-form" method="POST">
                     <div className="form-group">
                         <label for="name">Name</label>
-                        <input type="text" placeholder="Enter name" />
+                        <input 
+                        type="text" 
+                        placeholder="Enter name"
+                        className="form-control" />
                     </div>
                     <div className="form-group">
                         <label for="email">Email</label>
-                        <input type="email" placeholder="Enter Email" />
+                        <input 
+                        type="email" 
+                        placeholder="Enter Email"
+                        className="form-control" />
                     </div>
                     <div className="form-group">
                         <label for="message">Message</label>
-                        <input type="text" placeholder="Message" />
+                        <input 
+                        type="text" 
+                        placeholder="Message"
+                        className="form-control" />
                     </div>
                 </form>
             </div>
