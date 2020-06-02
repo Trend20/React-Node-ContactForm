8103c662974a42ee6ea999e7a8ea909a9ea96124
Trend20 <enockomondi305@gmail.com>
2020-06-02 08:59:28 -0400

    packages

--- src/config.js
+++ src/config.js
@@ -2,3 +2,14 @@ module.exports = {
     USER: 'YOUR_EMAIL_ADDRESS', 
     PASS: 'PASSWORD_FOR_EMAIL'
 }
+
+const express = require('express');
+const router = express.Router();
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const creds = require('./config');
+
+
+let transport = {
+    
+}
